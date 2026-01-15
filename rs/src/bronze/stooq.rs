007631da30f5dd733ba2ca5b8cc026a1c5569708
Maciej Wawrzynczuk <maciej.wawrzynczuk@gmail.com>
2026-01-15 10:53:51 +0000

    files

--- rs/src/bronze/stooq.rs
+++ rs/src/bronze/stooq.rs
@@ -2,8 +2,8 @@ use walkdir::WalkDir;
 use anyhow::Result;
 
 pub fn list_files() -> Result<()> {
-    for e in WalkDir::new(crate::DATA_BASE) {
-        println!("{}", e?.path().display());
+    for e in WalkDir::new(crate::DATA_BASE).into_iter().filter_map(|e| e.ok()).filter(|e| e.file_type().is_file()) {
+            println!("{}", e.path().display());
     }
     Ok(())
 }
