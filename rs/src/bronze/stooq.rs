e12db7d5e4e804c2778f19ea2ed3cde9e4424f82
Maciej Wawrzynczuk <maciej.wawrzynczuk@gmail.com>
2026-01-16 20:10:08 +0000

    mass download

--- rs/src/bronze/stooq.rs
+++ rs/src/bronze/stooq.rs
@@ -1,9 +1,10 @@
 use walkdir::WalkDir;
-use anyhow::Result;
+use std::path::PathBuf;
 
-pub fn list_files() -> Result<()> {
-    for e in WalkDir::new(crate::DATA_BASE).into_iter().filter_map(|e| e.ok()).filter(|e| e.file_type().is_file()) {
-            println!("{}", e.path().display());
-    }
-    Ok(())
+pub fn list_files() -> impl Iterator<Item = PathBuf> {
+    WalkDir::new(crate::DATA_BASE)
+        .into_iter()
+        .filter_map(|e| e.ok())
+        .filter(|e| e.file_type().is_file())
+        .map(|e| e.path().to_owned())
 }
--- rs/src/main.rs
+++ rs/src/main.rs
@@ -6,14 +6,19 @@ use crate::raw::stooq::stooq_download;
 use crate::bronze::stooq::list_files;
 use anyhow::Result;
 use env_logger;
+use futures::future::join_all;
 
 pub const DATA_BASE: &str = "/home/maciekw/proj/vigilant-waddle/data";
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let symbols = vec!["ads.de", "ibm.us"];
     env_logger::init();
-    stooq_download("ads.de").await?;
-    list_files()?;
+    let futs = symbols.into_iter().map(|s| stooq_download(s));
+    join_all(futs).await;
+    list_files()
+        .for_each(|f| println!("{}", f.display()));
+
     Ok(())
 }
 
