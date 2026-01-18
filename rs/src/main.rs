cff1bdea273be84eab4132448de811d745185ced
Maciej Wawrzynczuk <maciej.wawrzynczuk@gmail.com>
2026-01-18 16:18:05 +0000

    can read csv

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -1,12 +1,14 @@
-mod bronze;
 mod stooq_download;
 
 use crate::stooq_download::stooq_download;
-use crate::bronze::stooq::list_files;
-use anyhow::Result;
+use anyhow::{Result, Context};
 use env_logger;
 use futures::future::join_all;
 use std::path::PathBuf;
+use polars::prelude::*;
+
+// use polars_core::prelude::*;
+// use polars_io::prelude::*;
 
 pub const DATA_BASE: &str = "/home/maciekw/proj/vigilant-waddle/data";
 
@@ -21,10 +23,20 @@ async fn main() -> Result<()> {
     let futs = symbols.into_iter().map(|s| stooq_download(s, &base_path));
     let result = join_all(futs).await;
 
-    result.into_iter()
-        .filter_map(|r| r.ok())
-        .for_each(|i| println!("{}", i.display()));
+    let the_first_path = result.into_iter()
+        .filter_map(|x| x.ok())
+        .next()
+        .context("None downloaded")?;
+
+    let df = CsvReadOptions::default()
+        .with_has_header(true)
+        .try_into_reader_with_file_path(Some(the_first_path))?
+        .finish()?;
+
+    let sh = df.schema();
+    let h = df.height();
+    println!("{sh:?}");
+    println!("{h}");
 
     Ok(())
 }
-
