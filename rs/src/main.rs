54432815737e9915850283883094743bc2e83afa
Maciej Wawrzynczuk <maciej.wawrzynczuk@gmail.com>
2026-01-19 17:15:55 +0000

    Proper schema

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -4,9 +4,10 @@ use crate::stooq_download::stooq_download;
 use anyhow::{Result, Context};
 use env_logger;
 use futures::future::join_all;
-use std::{io::Seek, path::PathBuf};
+use std:: path::PathBuf;
 use std::fs::File;
-use arrow::csv::{ReaderBuilder, reader::Format};
+use arrow::csv::ReaderBuilder;
+use arrow::datatypes::{Field, Schema, DataType};
 use std::sync::Arc;
 
 pub const DATA_BASE: &str = "/home/maciekw/proj/vigilant-waddle/data";
@@ -27,10 +28,16 @@ async fn main() -> Result<()> {
         .next()
         .context("None downloaded")?;
 
-    let mut f = File::open(&the_first_path)?;
+    let f = File::open(&the_first_path)?;
 
-    let (schema, _) = Format::default().infer_schema(&f, Some(100))?;
-    f.rewind()?;
+    let schema = Schema::new(vec![
+        Field::new("date", DataType::Date32, false),
+        Field::new("open", DataType::Float64, false),
+        Field::new("high", DataType::Float64, false),
+        Field::new("low", DataType::Float64, false),
+        Field::new("close", DataType::Float64, false),
+        Field::new("volume", DataType::Int64, false),
+    ]); 
 
     let mut rd = ReaderBuilder::new(Arc::new(schema))
         .with_header(true)
