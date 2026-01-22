8bd5079a7499e5fc58eb49d7090f5210d94fe0ef
Maciej Wawrzynczuk <maciej.wawrzynczuk@gmail.com>
2026-01-22 09:58:11 +0000

    refactor2

--- rs/src/main.rs
+++ rs/src/main.rs
@@ -5,10 +5,11 @@ use anyhow::{Context, Result};
 use env_logger;
 use log;
 use futures::future::join_all;
-use std:: path::PathBuf;
+use std:: path::{Path, PathBuf};
 use std::fs::File;
 use arrow::csv::ReaderBuilder;
 use arrow::datatypes::{Field, Schema, DataType};
+use arrow::record_batch::RecordBatch;
 use std::sync::Arc;
 
 pub const DATA_BASE: &str = "/home/maciekw/proj/vigilant-waddle/data";
@@ -21,31 +22,13 @@ async fn main() -> Result<()> {
         .await
         .next().with_context(|| "None downloaded")?;
 
-    let f = File::open(&the_first_path)?;
 
-    let schema = Schema::new(vec![
-        Field::new("date", DataType::Date32, false),
-        Field::new("open", DataType::Float64, false),
-        Field::new("high", DataType::Float64, false),
-        Field::new("low", DataType::Float64, false),
-        Field::new("close", DataType::Float64, false),
-        Field::new("volume", DataType::Int64, false),
-    ]); 
-
-    let mut rd = ReaderBuilder::new(Arc::new(schema))
-        .with_header(true)
-        .build(&f)?;
-
-    while let Some(b) = rd.next() {
-        let b = b?;
-        println!("{b:?}");
-    }
-
-    println!("{}", the_first_path.display());
+   mk_arrow(&the_first_path)?
+       .iter()
+       .for_each(|br| println!("{}", br.num_rows()));
 
     Ok(())
 }
-
 
 async fn download_example() -> impl Iterator<Item = PathBuf> {
     let base_path = PathBuf::from(DATA_BASE)
@@ -66,4 +49,33 @@ async fn download_example() -> impl Iterator<Item = PathBuf> {
                 }
             }
         })
+}
+
+fn mk_arrow(p: &Path) -> Result<Vec<RecordBatch>> {
+
+    let f = File::open(p)?;
+
+    let schema = Schema::new(vec![
+        Field::new("date", DataType::Date32, false),
+        Field::new("open", DataType::Float64, false),
+        Field::new("high", DataType::Float64, false),
+        Field::new("low", DataType::Float64, false),
+        Field::new("close", DataType::Float64, false),
+        Field::new("volume", DataType::Int64, false),
+    ]); 
+
+    let rd = ReaderBuilder::new(Arc::new(schema))
+        .with_header(true)
+        .build(&f)?;
+
+    Ok(rd.filter_map(|r| {
+        match r {
+            Ok(br) => Some(br),
+            Err(e) => {
+                log::error!("{e}");
+                None
+            }
+        }
+    })
+    .collect())
 }
