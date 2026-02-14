ba56a0f551433ae002396a4a89731a240d5758e0
Maciej Wawrzynczuk <maciej.wawrzynczuk@gmail.com>
2026-02-14 17:11:52 +0100

    short sha
    short sha

--- src/ws.rs
+++ src/ws.rs
@@ -2,9 +2,7 @@ use axum::{Router, routing::get};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
+    let app = Router::new().route("/", get(|| async { "Hello, World!\n" }));
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
