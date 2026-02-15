80b41097643989c6cc467f6907f343659bb5044b
Maciej Wawrzynczuk <maciej.wawrzynczuk@gmail.com>
2026-02-15 07:34:50 +0100

    listen port from env var

--- src/ws.rs
+++ src/ws.rs
@@ -1,10 +1,17 @@
+use std::env;
+
 use axum::{Router, routing::get};
 use log::info;
 
 #[tokio::main]
 async fn main() {
+    let listen_addr = match env::var_os("WADDLE_LISTEN_PORT") {
+        Some(a) => a.into_string().unwrap(),
+        None => "127.0.0.1:3000".to_string(),
+    };
+
     let app = Router::new().route("/", get(|| async { "Hello, World!\n" }));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(listen_addr).await.unwrap();
     axum::serve(listener, app)
         .with_graceful_shutdown(shutdown_signal())
         .await
