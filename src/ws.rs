03e6b3c74fb2e0bde5aa9580afe3d2b3b1e25348
Maciej Wawrzynczuk <maciej.wawrzynczuk@gmail.com>
2026-02-15 07:10:13 +0100

    ctrl-c handle

--- src/ws.rs
+++ src/ws.rs
@@ -1,8 +1,19 @@
 use axum::{Router, routing::get};
+use log::info;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello, World!\n" }));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
+}
+
+async fn shutdown_signal() {
+    tokio::signal::ctrl_c()
+        .await
+        .expect("Failed to install Ctrl-c handler");
+    info!("Ctr-c pressed");
 }
